d4213aa5c8be828ef1469c3c3308930ac79cbc5e
Flavio Oliveira <flaviocfo@gmail.com>
2026-05-20 06:08:33 +0100

    test(index): port TestNewestSegment as degraded stub
    Sprint 58, GOC-4191.

---
+++ index/newest_segment_test.go
@@ -0,0 +1,41 @@
+// Copyright 2026 Gocene. All rights reserved.
+// Use of this source code is governed by the Apache License 2.0
+// that can be found in the LICENSE file.
+
+// Package index_test contains tests for the index package.
+//
+// Ported from Apache Lucene's org.apache.lucene.index.TestNewestSegment.
+//
+// GOC-4191: Index Tests - TestNewestSegment
+package index_test
+
+import (
+	"testing"
+
+	"github.com/FlavioCFOliveira/Gocene/index"
+	"github.com/FlavioCFOliveira/Gocene/store"
+)
+
+// TestNewestSegment verifies that a freshly created IndexWriter, before any
+// flush, reports no newest segment.
+func TestNewestSegment(t *testing.T) {
+	t.Skip("Sprint 55 option c: needs IndexWriter.newestSegment (not yet ported)")
+
+	dir := store.NewByteBuffersDirectory()
+	defer dir.Close()
+
+	config := index.NewIndexWriterConfig(createTestAnalyzer())
+	writer, err := index.NewIndexWriter(dir, config)
+	if err != nil {
+		t.Fatalf("NewIndexWriter() error = %v", err)
+	}
+
+	// IndexWriter.newestSegment() must be nil before the first flush.
+	// if seg := writer.NewestSegment(); seg != nil {
+	// 	t.Errorf("NewestSegment() = %v, want nil", seg)
+	// }
+
+	if err := writer.Close(); err != nil {
+		t.Fatalf("Close() error = %v", err)
+	}
+}
