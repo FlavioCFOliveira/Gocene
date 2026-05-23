3441bdce0de3614eb94a2058182d97455b89ecbb
Flavio Oliveira <flaviocfo@gmail.com>
2026-05-23 20:50:11 +0100

    feat(queries/mlt): port MoreLikeThisQuery replacing Sprint 29 stub

--- queries/mlt/mlt_overflow.go
+++ queries/mlt/mlt_overflow.go
@@ -1,20 +1,2 @@
-// Package mlt hosts the Sprint 29 overflow ports for
-// org.apache.lucene.queries.mlt.
+// Package mlt provides the Go port of org.apache.lucene.queries.mlt.
 package mlt
-
-// The Sprint 29 queries-module overflow surfaces these types as typed
-// stubs so dependent packages keep compiling; concrete behaviour ports
-// land progressively.
-
-// MoreLikeThis mirrors org.apache.lucene.queries.mlt.MoreLikeThis.
-type MoreLikeThis struct{}
-
-// NewMoreLikeThis builds a MoreLikeThis.
-func NewMoreLikeThis() *MoreLikeThis { return &MoreLikeThis{} }
-
-// MoreLikeThisQuery mirrors org.apache.lucene.queries.mlt.MoreLikeThisQuery.
-type MoreLikeThisQuery struct{}
-
-// NewMoreLikeThisQuery builds a MoreLikeThisQuery.
-func NewMoreLikeThisQuery() *MoreLikeThisQuery { return &MoreLikeThisQuery{} }
-
