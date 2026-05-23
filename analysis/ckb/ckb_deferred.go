f043abd372c3358fd102e9d4616cfd4c04d3f919
Flavio Oliveira <flaviocfo@gmail.com>
2026-05-23 10:56:38 +0100

    feat(analysis/ckb): implement SoraniAnalyzer replacing deferred stub
    Port org.apache.lucene.analysis.ckb.SoraniAnalyzer from Lucene 10.4.0.
    Add SetKeywordMarkerFilterFactoryWithSet for stem-exclusion support.

--- analysis/ckb/ckb_deferred.go
+++ analysis/ckb/ckb_deferred.go
@@ -1,13 +1,6 @@
-// Package ckb hosts the deferred Sprint 28 ports for
-// org.apache.lucene.analysis.ckb.
+// Copyright 2026 Gocene. All rights reserved.
+// Use of this source code is governed by the Apache License 2.0
+// that can be found in the LICENSE file.
+
+// Package ckb provides analysis components for Sorani Kurdish.
 package ckb
-
-// The Sprint 28 analysis-common deferral surfaces these types as typed
-// stubs so dependent packages keep compiling; concrete behaviour ports
-// land progressively.
-
-// SoraniAnalyzer mirrors org.apache.lucene.analysis.ckb.SoraniAnalyzer.
-type SoraniAnalyzer struct{}
-
-// NewSoraniAnalyzer builds a SoraniAnalyzer.
-func NewSoraniAnalyzer() *SoraniAnalyzer { return &SoraniAnalyzer{} }
