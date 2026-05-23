b8da93ea11cd9a8501cc5fcb5f34afc18d97f03f
Flavio Oliveira <flaviocfo@gmail.com>
2026-05-23 11:01:49 +0100

    feat(analysis/miscellaneous): implement deferred miscellaneous filter stubs
    Port ConcatenateGraphFilter, ConcatenateGraphFilterFactory, ConcatenatingTokenStream,
    ProtectedTermFilter, ProtectedTermFilterFactory, ConditionalTokenFilterFactory,
    StemmerOverrideFilter, StemmerOverrideFilterFactory from Lucene 10.4.0.
    Replace Sprint 28 stubs with full implementations in analysis/miscellaneous/.

--- analysis/miscellaneous/miscellaneous_deferred.go
+++ analysis/miscellaneous/miscellaneous_deferred.go
@@ -1,55 +1,6 @@
-// Package miscellaneous hosts the deferred Sprint 28 ports for
-// org.apache.lucene.analysis.miscellaneous.
+// Copyright 2026 Gocene. All rights reserved.
+// Use of this source code is governed by the Apache License 2.0
+// that can be found in the LICENSE file.
+
+// Package miscellaneous provides miscellaneous analysis filters and utilities.
 package miscellaneous
-
-// The Sprint 28 analysis-common deferral surfaces these types as typed
-// stubs so dependent packages keep compiling; concrete behaviour ports
-// land progressively.
-
-// ConcatenateGraphFilter mirrors org.apache.lucene.analysis.miscellaneous.ConcatenateGraphFilter.
-type ConcatenateGraphFilter struct{}
-
-// NewConcatenateGraphFilter builds a ConcatenateGraphFilter.
-func NewConcatenateGraphFilter() *ConcatenateGraphFilter { return &ConcatenateGraphFilter{} }
-
-// ConcatenateGraphFilterFactory mirrors org.apache.lucene.analysis.miscellaneous.ConcatenateGraphFilterFactory.
-type ConcatenateGraphFilterFactory struct{}
-
-// NewConcatenateGraphFilterFactory builds a ConcatenateGraphFilterFactory.
-func NewConcatenateGraphFilterFactory() *ConcatenateGraphFilterFactory {
-	return &ConcatenateGraphFilterFactory{}
-}
-
-// ConcatenatingTokenStream mirrors org.apache.lucene.analysis.miscellaneous.ConcatenatingTokenStream.
-type ConcatenatingTokenStream struct{}
-
-// NewConcatenatingTokenStream builds a ConcatenatingTokenStream.
-func NewConcatenatingTokenStream() *ConcatenatingTokenStream { return &ConcatenatingTokenStream{} }
-
-// ProtectedTermFilter mirrors org.apache.lucene.analysis.miscellaneous.ProtectedTermFilter.
-type ProtectedTermFilter struct{}
-
-// NewProtectedTermFilter builds a ProtectedTermFilter.
-func NewProtectedTermFilter() *ProtectedTermFilter { return &ProtectedTermFilter{} }
-
-// ProtectedTermFilterFactory mirrors org.apache.lucene.analysis.miscellaneous.ProtectedTermFilterFactory.
-type ProtectedTermFilterFactory struct{}
-
-// NewProtectedTermFilterFactory builds a ProtectedTermFilterFactory.
-func NewProtectedTermFilterFactory() *ProtectedTermFilterFactory {
-	return &ProtectedTermFilterFactory{}
-}
-
-// StemmerOverrideFilter mirrors org.apache.lucene.analysis.miscellaneous.StemmerOverrideFilter.
-type StemmerOverrideFilter struct{}
-
-// NewStemmerOverrideFilter builds a StemmerOverrideFilter.
-func NewStemmerOverrideFilter() *StemmerOverrideFilter { return &StemmerOverrideFilter{} }
-
-// StemmerOverrideFilterFactory mirrors org.apache.lucene.analysis.miscellaneous.StemmerOverrideFilterFactory.
-type StemmerOverrideFilterFactory struct{}
-
-// NewStemmerOverrideFilterFactory builds a StemmerOverrideFilterFactory.
-func NewStemmerOverrideFilterFactory() *StemmerOverrideFilterFactory {
-	return &StemmerOverrideFilterFactory{}
-}
