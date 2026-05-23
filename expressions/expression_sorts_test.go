6be4e8e9fc382aeb97cf5992839837c1cfee29ce
Flavio Oliveira <flaviocfo@gmail.com>
2026-05-24 00:59:29 +0100

    test(expressions): stub TestExpressionSorts (deferred)
    Port org.apache.lucene.expressions.TestExpressionSorts as a skip stub.
    Requires full ANTLR JavascriptCompiler and IndexSearcher.

---
+++ expressions/expression_sorts_test.go
@@ -0,0 +1,16 @@
+// Copyright 2026 Gocene. All rights reserved.
+// Use of this source code is governed by the Apache License 2.0
+// that can be found in the LICENSE file.
+
+// Port of org.apache.lucene.expressions.TestExpressionSorts.
+// Tests require JavascriptCompiler.compile (full ANTLR grammar) and a
+// full IndexSearcher with NumericDocValuesField. Deferred.
+package expressions_test
+
+import "testing"
+
+// TestExpressionSorts skips because it requires full ANTLR JavascriptCompiler
+// and IndexSearcher infrastructure not yet in Gocene.
+func TestExpressionSorts(t *testing.T) {
+	t.Skip("requires full ANTLR JavascriptCompiler and IndexSearcher infrastructure (not yet ported)")
+}
