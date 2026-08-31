490fab18cdc04b498971495c1368c6b6e6a3fe88
flavio cf oliveira <geral@flaviooliveira.com>
2026-08-31 20:31:52 +0100

    feat(index): port DocValuesWriter

---
+++ index/doc_values_writer.go
@@ -0,0 +1,15 @@
+package index
+
+import (
+	"github.com/FlavioCFOliveira/Gocene/spi"
+	"github.com/FlavioCFOliveira/Gocene/util"
+)
+
+// DocValuesWriter is a port of org.apache.lucene.index.DocValuesWriter.
+// It provides a common interface for writing doc values to a segment.
+type DocValuesWriter[T util.DocIdSetIterator] interface {
+	// Flush writes the doc values to the consumer.
+	Flush(state *spi.SegmentWriteState, sortMap spi.SorterDocMap, consumer spi.DocValuesConsumer) error
+	// GetDocValues returns the doc values as a DocIdSetIterator.
+	GetDocValues() T
+}
