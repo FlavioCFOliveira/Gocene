d20a1f1d2dbc036b51054e643c7d0b8d9b3216ca
flavio cf oliveira <geral@flaviooliveira.com>
2026-09-01 19:56:17 +0100

    feat(codecs): port STIntersectBlockReader skeleton

---
+++ codecs/uniformsplit/sharedterms/st_intersect_block_reader.go
@@ -0,0 +1,41 @@
+package sharedterms
+
+import (
+	"github.com/FlavioCFOliveira/Gocene/codecs/uniformsplit"
+	"github.com/FlavioCFOliveira/Gocene/index"
+	"github.com/FlavioCFOliveira/Gocene/store"
+	"github.com/FlavioCFOliveira/Gocene/util"
+)
+
+// STIntersectBlockReader intersects terms with an automaton.
+type STIntersectBlockReader struct {
+	*STBlockReader
+}
+
+// NewSTIntersectBlockReader builds an STIntersectBlockReader.
+func NewSTIntersectBlockReader(
+	compiled *util.CompiledAutomaton,
+	startTerm *util.BytesRef,
+	dictionaryBrowser any,
+	input store.DataInput,
+	postingsReader any,
+	fieldMetadata *uniformsplit.FieldMetadata,
+	decoder uniformsplit.BlockDecoder,
+	fieldInfos *index.FieldInfos) *STIntersectBlockReader {
+	
+	return &STIntersectBlockReader{
+		STBlockReader: NewSTBlockReader(
+			dictionaryBrowser,
+			input,
+			postingsReader,
+			fieldMetadata,
+			decoder,
+			fieldInfos),
+	}
+}
+
+// Next returns the next intersecting term.
+func (r *STIntersectBlockReader) Next() (*util.BytesRef, error) {
+	// Logic to intersect with automaton and filter by field
+	return nil, nil // Placeholder
+}
