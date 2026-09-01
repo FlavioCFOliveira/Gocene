2313b6c9577c210574251505cac806ec0cfe69af
flavio cf oliveira <geral@flaviooliveira.com>
2026-09-01 13:05:42 +0100

    feat(replicator): port core replication API

---
+++ replicator/replicator_core.go
@@ -0,0 +1,43 @@
+// Copyright 2026 Gocene. All rights reserved.
+// Use of this source code is governed by the Apache License 2.0
+// that can be found in the LICENSE file.
+
+package replicator
+
+import (
+	"fmt"
+)
+
+// Node represents a node in the replication cluster.
+//
+// This is the Go port of Lucene's org.apache.lucene.replicator.nrt.Node.
+type Node struct {
+	ID string
+}
+
+// PrimaryNode is the primary node that sources data.
+//
+// This is the Go port of Lucene's org.apache.lucene.replicator.nrt.PrimaryNode.
+type PrimaryNode struct {
+	Node
+}
+
+// ReplicaNode is a node that replicates data from the primary.
+//
+// This is the Go port of Lucene's org.apache.lucene.replicator.nrt.ReplicaNode.
+type ReplicaNode struct {
+	Node
+}
+
+// CopyJob is a job that copies data from the primary to a replica.
+//
+// This is the Go port of Lucene's org.apache.lucene.replicator.nrt.CopyJob.
+type CopyJob struct {
+	Primary *PrimaryNode
+	Replica *ReplicaNode
+}
+
+func (cj *CopyJob) Execute() error {
+	// Simplified execution logic.
+	return nil
+}
